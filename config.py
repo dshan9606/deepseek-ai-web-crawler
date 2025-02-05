69ed9ed52118e2639314bb3995718927e018a563
dshan9606 <91290852+dshan9606@users.noreply.github.com>
2025-02-05 18:40:38 -0500

    First commit
    First commit

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # config.py
+# This file contains the configuration for the web crawler.
+#https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
 
-BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
+BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-albany-ny"
 CSS_SELECTOR = "[class^='info-container']"
+
 REQUIRED_KEYS = [
     "name",
     "price",
